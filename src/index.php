d820dc11c569e2d6424fb3e26ec1a53742f3f0dd
pro-ghanem <holywater150@gmail.com>
2020-09-05 22:02:59 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,31 +1,20 @@
 <?php
-echo " Welcome to Ghanem Test<br>";
-$conn = new mysqli("mysql", "db_user", "ghanem", "my_db");
+echo "Inside K8s with MySQL <br>";
+$conn = new mysqli("mysql8-service", "root", ".sweetpwd.", "my_db");
 // Check connection
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
 
+$sql = "SELECT name FROM users";
+$result = $conn->query($sql);
 
-
-// Create database
-$sql = "CREATE DATABASE ghanem";
-if ($conn->query($sql) === TRUE) {
-  echo "Database created successfully";
+if ($result->num_rows > 0) {
+	// output data of each row
+	while($row = $result->fetch_assoc()) {
+		echo $row['name']."<br>";
+	}
 } else {
-  echo "Error creating database: " . $conn->error;
+	echo "0 results";
 }
-      
-
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('ahmed', 'ghanem', 'pro.ahmedghanem@gmail.com')";
-
-if ($conn->query($sql) === TRUE) {
-  $last_id = $conn->insert_id;
-  echo "New record created successfully. Last inserted ID is: " . $last_id;
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-
 $conn->close();

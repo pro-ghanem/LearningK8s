3e9e9310c02bf95c4fc35cfcbc38ca537608cbb9
pro-ghanem <holywater150@gmail.com>
2020-09-05 21:43:28 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -6,15 +6,26 @@ if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT name FROM users";
-$result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-	// output data of each row
-	while($row = $result->fetch_assoc()) {
-		echo $row['name']."<br>";
-	}
+
+// Create database
+$sql = "CREATE DATABASE ghanem";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
 } else {
-	echo "0 results";
+  echo "Error creating database: " . $conn->error;
 }
+      
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('ahmed', 'ghanem', 'pro.ahmedghanem@gmail.com')";
+
+if ($conn->query($sql) === TRUE) {
+  $last_id = $conn->insert_id;
+  echo "New record created successfully. Last inserted ID is: " . $last_id;
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+
 $conn->close();
